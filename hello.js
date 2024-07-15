57ef5c054305480f4f2112dd3cc670fad817f5ea
sakib531 <mdsakibahammad531@gmail.com>
2024-07-15 21:42:55 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,16 @@
+const product = {
+    title: "Ball Pen",
+    rating :4 ,
+    offer : 5,
+    price : 270,
+};
+
+console.log(product);
+
+const profile = {
+    userName : "Md. Sakib Ahammad",
+    isFollow : false ,
+    folowing :123,
+};
+console.log(typeof profile);
+console.log(typeof profile["userName"]);
